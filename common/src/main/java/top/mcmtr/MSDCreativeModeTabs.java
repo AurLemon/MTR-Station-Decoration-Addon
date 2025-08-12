399b4281e269dcecd6aee5e771a196179dddd12e
AurLemon <aurlemoncn@gmail.com>
2025-08-12 13:38:13 +0800

    fix(locales): 修复物品组命名并更新翻译文件

--- common/src/main/java/top/mcmtr/MSDCreativeModeTabs.java
+++ common/src/main/java/top/mcmtr/MSDCreativeModeTabs.java
@@ -7,6 +7,6 @@ import net.minecraft.world.item.ItemStack;
 import static top.mcmtr.MSDMain.MOD_ID;
 
 public interface MSDCreativeModeTabs {
-    CreativeModeTabs.Wrapper MSD_BLOCKS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "msd_blocks"), () -> new ItemStack(MSDBlocks.RAILING_STAIR_FLAT.get()));
-    CreativeModeTabs.Wrapper MSD_Station_Decoration = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "msd_station_blocks"), () -> new ItemStack(MSDBlocks.DECORATION_STAIR.get()));
+    CreativeModeTabs.Wrapper MSD_BLOCKS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "blocks"), () -> new ItemStack(MSDBlocks.RAILING_STAIR_FLAT.get()));
+    CreativeModeTabs.Wrapper MSD_Station_Decoration = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "station_blocks"), () -> new ItemStack(MSDBlocks.DECORATION_STAIR.get()));
 }
